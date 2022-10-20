34b8f6a6386c0eb57ed662624737bcc442518ac8
Saul Pierotti <saul@ebi.ac.uk>
2022-10-20 10:52:01 +0100

    advanced

--- docs/nextflow_workshop_ebi_predoc_course_2022/Nextflow Tower_files/color-scheme-listener.js
+++
@@ -1,8 +0,0 @@
-const mediaMatcher = window.matchMedia('(prefers-color-scheme: dark)');
-function onColorSchemeUpdate() {
-  let link = document.getElementById('faviconLink');
-  link.href = mediaMatcher.matches ? '/assets/favicon.white.png' : '/favicon.ico';
-}
-
-mediaMatcher.addEventListener("change", onColorSchemeUpdate);
-onColorSchemeUpdate();
